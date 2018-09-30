06c0218e199acba8da0fa7d7b3fb34ea20403c2f
vonzhou <vonzhou@163.com>
2018-09-30 14:16:33 +0800

    3. listStatus

--- ch03-hdfs/src/main/java/ListStatus.java
+++ ch03-hdfs/src/main/java/ListStatus.java
@@ -1,4 +1,5 @@
 // cc ListStatus Shows the file statuses for a collection of paths in a Hadoop filesystem 
+
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
@@ -7,24 +8,22 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 
-// vv ListStatus
 public class ListStatus {
 
-  public static void main(String[] args) throws Exception {
-    String uri = args[0];
-    Configuration conf = new Configuration();
-    FileSystem fs = FileSystem.get(URI.create(uri), conf);
-    
-    Path[] paths = new Path[args.length];
-    for (int i = 0; i < paths.length; i++) {
-      paths[i] = new Path(args[i]);
+    public static void main(String[] args) throws Exception {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+
+        Path[] paths = new Path[args.length];
+        for (int i = 0; i < paths.length; i++) {
+            paths[i] = new Path(args[i]);
+        }
+
+        FileStatus[] status = fs.listStatus(paths);
+        Path[] listedPaths = FileUtil.stat2Paths(status);
+        for (Path p : listedPaths) {
+            System.out.println(p);
+        }
     }
-    
-    FileStatus[] status = fs.listStatus(paths);
-    Path[] listedPaths = FileUtil.stat2Paths(status);
-    for (Path p : listedPaths) {
-      System.out.println(p);
-    }
-  }
 }
-// ^^ ListStatus
