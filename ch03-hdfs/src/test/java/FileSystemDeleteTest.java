2d0efa6d025b880c8b9728efce7579df2f710169
vonzhou <vonzhou@163.com>
2018-09-30 14:44:52 +0800

    3. test

--- ch03-hdfs/src/test/java/FileSystemDeleteTest.java
+++ ch03-hdfs/src/test/java/FileSystemDeleteTest.java
@@ -12,42 +12,42 @@ import org.junit.Test;
 
 public class FileSystemDeleteTest {
 
-  private FileSystem fs;
-  
-  @Before
-  public void setUp() throws Exception {
-    fs = FileSystem.get(new Configuration());
-    writeFile(fs, new Path("dir/file"));
-  }
-  
-  private void writeFile(FileSystem fileSys, Path name) throws IOException {
-    FSDataOutputStream stm = fileSys.create(name);
-    stm.close();
-  }
-  
-  @Test
-  public void deleteFile() throws Exception {
-    assertThat(fs.delete(new Path("dir/file"), false), is(true));
-    assertThat(fs.exists(new Path("dir/file")), is(false));
-    assertThat(fs.exists(new Path("dir")), is(true));
-    assertThat(fs.delete(new Path("dir"), false), is(true));
-    assertThat(fs.exists(new Path("dir")), is(false));
-  }
+    private FileSystem fs;
 
-  @Test
-  public void deleteNonEmptyDirectoryNonRecursivelyFails() throws Exception {
-    try {
-      fs.delete(new Path("dir"), false);
-      fail("Shouldn't delete non-empty directory");
-    } catch (IOException e) {
-      // expected
+    @Before
+    public void setUp() throws Exception {
+        fs = FileSystem.get(new Configuration());
+        writeFile(fs, new Path("dir/file"));
     }
-  }
-  
-  @Test
-  public void deleteDirectory() throws Exception {
-    assertThat(fs.delete(new Path("dir"), true), is(true));
-    assertThat(fs.exists(new Path("dir")), is(false));
-  }
-  
+
+    private void writeFile(FileSystem fileSys, Path name) throws IOException {
+        FSDataOutputStream stm = fileSys.create(name);
+        stm.close();
+    }
+
+    @Test
+    public void deleteFile() throws Exception {
+        assertThat(fs.delete(new Path("dir/file"), false), is(true));
+        assertThat(fs.exists(new Path("dir/file")), is(false));
+        assertThat(fs.exists(new Path("dir")), is(true));
+        assertThat(fs.delete(new Path("dir"), false), is(true));
+        assertThat(fs.exists(new Path("dir")), is(false));
+    }
+
+    @Test
+    public void deleteNonEmptyDirectoryNonRecursivelyFails() throws Exception {
+        try {
+            fs.delete(new Path("dir"), false);
+            fail("Shouldn't delete non-empty directory");
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void deleteDirectory() throws Exception {
+        assertThat(fs.delete(new Path("dir"), true), is(true));
+        assertThat(fs.exists(new Path("dir")), is(false));
+    }
+
 }
