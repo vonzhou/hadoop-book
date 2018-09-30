f2a2170f0b45bb18227ba5515d8f28550517247b
vonzhou <vonzhou@163.com>
2018-09-30 14:21:22 +0800

    3. PathFilter

--- ch03-hdfs/src/main/java/DateRangePathFilter.java
+++ ch03-hdfs/src/main/java/DateRangePathFilter.java
@@ -9,31 +9,30 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
 
 public class DateRangePathFilter implements PathFilter {
-  
-  private final Pattern PATTERN = Pattern.compile("^.*/(\\d\\d\\d\\d/\\d\\d/\\d\\d).*$");
-  
-  private final Date start, end;
 
-  public DateRangePathFilter(Date start, Date end) {
-    this.start = new Date(start.getTime());
-    this.end = new Date(end.getTime());
-  }
-  
-  public boolean accept(Path path) {
-    Matcher matcher = PATTERN.matcher(path.toString());
-    if (matcher.matches()) {
-      DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-      try {
-        return inInterval(format.parse(matcher.group(1)));
-      } catch (ParseException e) {
-        return false;
-      }
+    private final Pattern PATTERN = Pattern.compile("^.*/(\\d\\d\\d\\d/\\d\\d/\\d\\d).*$");
+
+    private final Date start, end;
+
+    public DateRangePathFilter(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
     }
-    return false;
-  }
 
-  private boolean inInterval(Date date) {
-    return !date.before(start) && !date.after(end);
-  }
+    public boolean accept(Path path) {
+        Matcher matcher = PATTERN.matcher(path.toString());
+        if (matcher.matches()) {
+            DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+            try {
+                return inInterval(format.parse(matcher.group(1)));
+            } catch (ParseException e) {
+                return false;
+            }
+        }
+        return false;
+    }
 
+    private boolean inInterval(Date date) {
+        return !date.before(start) && !date.after(end);
+    }
 }
--- ch03-hdfs/src/main/java/RegexPathFilter.java
+++ ch03-hdfs/src/main/java/RegexPathFilter.java
@@ -2,21 +2,21 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
 
 public class RegexPathFilter implements PathFilter {
-  
-  private final String regex;
-  private final boolean include;
 
-  public RegexPathFilter(String regex) {
-    this(regex, true);
-  }
-  
-  public RegexPathFilter(String regex, boolean include) {
-    this.regex = regex;
-    this.include = include;
-  }
+    private final String regex;
+    private final boolean include;
 
-  public boolean accept(Path path) {
-    return (path.toString().matches(regex)) ? include : !include;
-  }
+    public RegexPathFilter(String regex) {
+        this(regex, true);
+    }
+
+    public RegexPathFilter(String regex, boolean include) {
+        this.regex = regex;
+        this.include = include;
+    }
+
+    public boolean accept(Path path) {
+        return (path.toString().matches(regex)) ? include : !include;
+    }
 
 }
