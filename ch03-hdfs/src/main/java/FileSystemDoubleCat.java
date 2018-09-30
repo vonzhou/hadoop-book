002c6db7259e872bcb73c1ca4496184c399b6bdd
vonzhou <vonzhou@163.com>
2018-09-30 11:12:45 +0800

    3. FSDataInputStream support random access

--- ch03-hdfs/src/main/java/FileSystemDoubleCat.java
+++ ch03-hdfs/src/main/java/FileSystemDoubleCat.java
@@ -1,4 +1,5 @@
 // cc FileSystemDoubleCat Displays files from a Hadoop filesystem on standard output twice, by using seek
+
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
@@ -7,22 +8,21 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
-// vv FileSystemDoubleCat
+// P60
 public class FileSystemDoubleCat {
 
-  public static void main(String[] args) throws Exception {
-    String uri = args[0];
-    Configuration conf = new Configuration();
-    FileSystem fs = FileSystem.get(URI.create(uri), conf);
-    FSDataInputStream in = null;
-    try {
-      in = fs.open(new Path(uri));
-      IOUtils.copyBytes(in, System.out, 4096, false);
-      in.seek(0); // go back to the start of the file
-      IOUtils.copyBytes(in, System.out, 4096, false);
-    } finally {
-      IOUtils.closeStream(in);
+    public static void main(String[] args) throws Exception {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        FSDataInputStream in = null;
+        try {
+            in = fs.open(new Path(uri));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+            in.seek(0); // go back to the start of the file
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
     }
-  }
 }
-// ^^ FileSystemDoubleCat
