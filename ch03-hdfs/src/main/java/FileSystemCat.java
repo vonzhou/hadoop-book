e1391dd387469e8796c5fec410e164cc1dbe0b49
vonzhou <vonzhou@163.com>
2018-09-30 11:07:11 +0800

    3. Reading data using FileSystem API

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -1,4 +1,5 @@
 // cc FileSystemCat Displays files from a Hadoop filesystem on standard output by using the FileSystem directly
+
 import java.io.InputStream;
 import java.net.URI;
 
@@ -7,20 +8,19 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
-// vv FileSystemCat
+// P59
 public class FileSystemCat {
 
-  public static void main(String[] args) throws Exception {
-    String uri = args[0];
-    Configuration conf = new Configuration();
-    FileSystem fs = FileSystem.get(URI.create(uri), conf);
-    InputStream in = null;
-    try {
-      in = fs.open(new Path(uri));
-      IOUtils.copyBytes(in, System.out, 4096, false);
-    } finally {
-      IOUtils.closeStream(in);
+    public static void main(String[] args) throws Exception {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        InputStream in = null;
+        try {
+            in = fs.open(new Path(uri));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
     }
-  }
 }
-// ^^ FileSystemCat
