4221a5f6c1e7909c00f0267930e83a201ba6154d
vonzhou <vonzhou@163.com>
2018-10-08 17:07:08 +0800

    6. 配置文件

--- ch06-mr-dev/src/test/java/SingleResourceConfigurationTest.java
+++ ch06-mr-dev/src/test/java/SingleResourceConfigurationTest.java
@@ -1,4 +1,5 @@
 // == SingleResourceConfigurationTest
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,16 +9,17 @@ import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
 
 public class SingleResourceConfigurationTest {
-  
-  @Test
-  public void get() throws IOException {
-    // vv SingleResourceConfigurationTest
-    Configuration conf = new Configuration();
-    conf.addResource("configuration-1.xml");
-    assertThat(conf.get("color"), is("yellow"));
-    assertThat(conf.getInt("size", 0), is(10));
-    assertThat(conf.get("breadth", "wide"), is("wide"));
-    // ^^ SingleResourceConfigurationTest
-  }
+
+    @Test
+    public void get() throws IOException {
+        // vv SingleResourceConfigurationTest
+        Configuration conf = new Configuration();
+        conf.addResource("configuration-1.xml");
+        System.out.println(conf.get("color"));
+        assertThat(conf.get("color"), is("yellow"));
+        assertThat(conf.getInt("size", 0), is(10));
+        assertThat(conf.get("breadth", "wide"), is("wide"));
+        // ^^ SingleResourceConfigurationTest
+    }
 
 }
